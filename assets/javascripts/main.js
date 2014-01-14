019bbd44b57d78c0926bf8a2a0a285bb0b876ab7
Hudson Marinho <hudsonmarinhodev@gmail.com>
2014-01-14 17:31:41 -0300

    scrollFooter() create function to verify scroll position and thus hide the footer.

---
+++ assets/javascripts/main.js
@@ -0,0 +1,18 @@
+//<![CDATA[
+// scrollFooter() create function to verify scroll position and thus hide the footer.
+function scrollFooter(scrollY, heightFooter)
+{
+	if(scrollY >= heightFooter)
+	{
+		$('footer').css({
+			'bottom' : '0px'
+		});
+	}
+	else
+	{
+		$('footer').css({
+			'bottom' : '-' + heightFooter + 'px'
+		});
+	}
+}
+//]]>
