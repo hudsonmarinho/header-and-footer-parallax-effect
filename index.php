a95b85a824e54c671b2020869eeea14a96fd24ac
Hudson Marinho <hudsonmarinhodev@gmail.com>
2014-01-14 16:36:33 -0300

    Add link to files .css
    On file index.php was created links for files ".css"

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!doctype html>
 <html lang="en">
 	<head>
+		
 		<meta charset="UTF-8">
 		<title>Header/Footer Parallax Effect</title>
+
+		<link rel="stylesheet" type="text/css" href="assets/stylesheets/normalize.css">
+		<link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+
 	</head>
 
 	<body>
